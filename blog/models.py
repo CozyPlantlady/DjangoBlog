41374c9492604ea525f88c520d97286854727d5a
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-10-14 11:45:57 +0000

    Test

--- blog/models.py
+++ blog/models.py
@@ -33,6 +33,7 @@ class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
+    body = models.TextField()
     email = models.EmailField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
@@ -41,4 +42,4 @@ class Comment(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return self.title
+        return f"Comment {self.body} by {self.name}"
